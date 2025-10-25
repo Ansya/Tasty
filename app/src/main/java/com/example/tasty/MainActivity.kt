0489e8dd954ffcec2c286f4664a3c5febb2057e0
ansya <anastasia.kuimova@gmail.com>
2025-10-25 15:49:36 +0300

    RA-03: navigation buttons added

---
+++ app/src/main/java/com/example/tasty/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tasty
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tasty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFavorites.text = "Избранное"
+    }
+}
